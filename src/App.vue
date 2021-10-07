706a4aa63217d226e31a1f311f250031e1828071
Alfredroshan555 <alfredroshan555@gmail.com>
2021-10-07 16:56:05 +0530

    Final deployment ready

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Reaction Timer Game</h1>
     <button @click="start" :disabled="isPlaying">Play</button>
-    <Block v-if="isPlaying" :delay="delay" />
+    <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+    <!-- <h4 v-if="showResults">Reaction Time : {{ score }}ms</h4> -->
+    <Results v-if="showResults" :score="score"/>
   </div>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Result.vue'
 
   export default{
     name: 'App',
-    components: { Block },
+    components: { Block,Results },
     data() {
       return {
         isPlaying: false,
-        delay: null
+        delay: null,
+        score: null,
+        showResults: false
       }
     },
     methods: {
@@ -23,6 +28,11 @@ import Block from './components/Block.vue'
         this.delay = 2000 + Math.random() * 5000
         this.isPlaying = true
         console.log(this.delay)
+      },
+      endGame(reactionTime){
+        this.score = reactionTime
+        this.isPlaying = false
+        this.showResults = true
       }
     },
   }
@@ -36,7 +46,35 @@ import Block from './components/Block.vue'
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #d1d7dd;
   margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: aliceblue;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  margin: 10px;
+  cursor: pointer;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+body{
+background: #000428;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #004e92, #000428);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #004e92, #000428); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+}
+.container{
+  border: 3px solid rgb(255, 255, 255);
+
+  
+  text-align: center;
+  align-content: center;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,36 @@
+<template>
+  <h4>Reaction Time - {{score}}ms</h4>
+  <h2 class="ranks">{{rank}}</h2>
+</template>
+
+<script>
+export default {
+props: ['score'],
+data() {
+    return {
+        rank: null
+    }
+    
+},
+mounted() {
+    if(this.score < 300){
+        this.rank = "Lightning Fast"
+    }else if(this.score <500){
+        this.rank = "Very Fast"
+    }else if(this.score <700){
+        this.rank = "Slow"
+    }else{
+        this.rank = "Too Slow"
+    }
+},
+}
+</script>
+
+<style>
+.ranks{
+    font-size: 1.4rem;
+    color: #0faf87;
+    font-weight: bold;
+}
+
+</style>
