5df94f70f41a82657056a87ab3e348e19b0a637d
Alfredroshan555 <alfredroshan555@gmail.com>
2021-10-07 14:54:24 +0530

    first update

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>hello</h1>
+    <h1>My name is alfred</h1>
+    <h3>hjksdfa</h3>
+    <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, perspiciatis. Quasi itaque exercitationem id nulla enim quibusdam qui nam. Ducimus sunt maiores iste ipsum cupiditate, soluta quisquam porro labore in!</h1>
+  </div>
+</template>
+
+<script>
+
+  export default{
+    component: { }
+  }
+  
+  
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
